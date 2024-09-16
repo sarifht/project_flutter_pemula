1b059bf6f3f105ab27012506e30473574a7c9376
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-09-17 06:20:21 +0700

    update app icon dan foto profile contact

--- lib/contact_provider.dart
+++ lib/contact_provider.dart
@@ -5,91 +5,91 @@ class ContactProvider with ChangeNotifier {
     {
       "name": "Aulus",
       "phone": "081234567890",
-      "image": "images/profile.png",
+      "image": "images/avatars/aulus.png",
       "isFavorite": false
     },
     {
       "name": "Bruno",
       "phone": "081234567891",
-      "image": "images/profile.png",
+      "image": "images/avatars/bruno.png",
       "isFavorite": false
     },
     {
       "name": "Claude",
       "phone": "081234567892",
-      "image": "images/profile.png",
+      "image": "images/avatars/claude.png",
       "isFavorite": false
     },
     {
       "name": "Diggie",
       "phone": "081234567893",
-      "image": "images/profile.png",
+      "image": "images/avatars/diggie.png",
       "isFavorite": false
     },
     {
       "name": "Eudora",
       "phone": "081234567894",
-      "image": "images/profile.png",
+      "image": "images/avatars/eudora.png",
       "isFavorite": false
     },
     {
       "name": "Fredrinn",
       "phone": "081234567895",
-      "image": "images/profile.png",
+      "image": "images/avatars/fredrinn.png",
       "isFavorite": false
     },
     {
       "name": "Gusion",
       "phone": "081234567896",
-      "image": "images/profile.png",
+      "image": "images/avatars/gusion.png",
       "isFavorite": false
     },
     {
       "name": "Hilda",
       "phone": "081234567897",
-      "image": "images/profile.png",
+      "image": "images/avatars/hilda.png",
       "isFavorite": false
     },
     {
       "name": "Ixia",
       "phone": "081234567898",
-      "image": "images/profile.png",
+      "image": "images/avatars/ixia.png",
       "isFavorite": false
     },
     {
       "name": "Joy",
       "phone": "081234567899",
-      "image": "images/profile.png",
+      "image": "images/avatars/joy.png",
       "isFavorite": false
     },
     {
       "name": "Karina",
       "phone": "081234567800",
-      "image": "images/profile.png",
+      "image": "images/avatars/karina.png",
       "isFavorite": false
     },
     {
       "name": "Ling",
       "phone": "081234567801",
-      "image": "images/profile.png",
+      "image": "images/avatars/ling.png",
       "isFavorite": false
     },
     {
       "name": "Miya",
       "phone": "081234567802",
-      "image": "images/profile.png",
+      "image": "images/avatars/miya.png",
       "isFavorite": false
     },
     {
       "name": "Nana",
       "phone": "081234567803",
-      "image": "images/profile.png",
+      "image": "images/avatars/nana.png",
       "isFavorite": false
     },
     {
       "name": "Odette",
       "phone": "081234567804",
-      "image": "images/profile.png",
+      "image": "images/avatars/odette.png",
       "isFavorite": false
     },
     // Tambahkan kontak lainnya
